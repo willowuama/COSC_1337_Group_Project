0d7e8a774fbb332b357c009ad60cffbaaab09885
Will Owuama <willowuama@gmail.com>
2019-11-19 15:24:37 -0600

    Added MovingTriangle to separate file

---
+++ src/MovingTriangle.java
@@ -0,0 +1,15 @@
+public class MovingTriangle extends MovingObject {
+
+	// Constructor
+	public MovingTriangle(){
+		Polygon triangle = new Polygon(100, 50, 150, 0, 200, 50); // May need to change
+		triangle.setStroke(Color.BLACK);
+		triangle.setFill(Color.RED);
+		getChildren().add(triangle);
+	}
+
+	@Override
+	public void playMovingObject(MovingObject movingObject){
+		// TODO Auto-generated method stub
+	}
+}
